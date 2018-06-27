8838a3e2a2cce7410f3952f780a942c534b64d58
yalonsob <yalonsob@gmail.com>
2018-06-27 09:58:02 -0700

    Integrate dates

--- js/notes-app.js
+++ js/notes-app.js
@@ -1,17 +1,21 @@
 let notes = getSavedNotes();
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    sortBy: 'byEdited'
 };
 
 renderNotes(notes, filters);
 
 document.querySelector('#create-note').addEventListener('click', function(e){
     const id = uuidv4();
+    const timestamp = moment().valueOf();
     notes.push({
         id: id,
         title: '',
-        body: ''
+        body: '',
+        createdAt: timestamp,
+        updatedAt: timestamp
     });
     saveNotes(notes);
     location.assign(`/edit.html#${id}`);
@@ -23,7 +27,8 @@ document.querySelector('#search-text').addEventListener('input', function(e) {
 });
 
 document.querySelector('#filter-by').addEventListener('change', function(e) {
-    console.log(e.target.value);
+    filters.sortBy = e.target.value;
+    renderNotes(notes, filters);
 });
 
 window.addEventListener('storage', function(e) {
@@ -32,4 +37,6 @@ window.addEventListener('storage', function(e) {
         renderNotes(notes, filters);
     }
 });
+
+
 
--- js/notes-edit.js
+++ js/notes-edit.js
@@ -1,6 +1,7 @@
 const noteTitleElement = document.querySelector('#note-title');
 const noteBodyElement = document.querySelector('#note-body');
 const removeNoteButton = document.querySelector('#remove-note');
+const dateElement = document.querySelector('#last-edited');
 const noteId = location.hash.substring(1);
 let notes = getSavedNotes();
 let note = notes.find(function(note) {
@@ -13,14 +14,19 @@ if(note === undefined) {
 
 noteTitleElement.value = note.title;
 noteBodyElement.value = note.body;
+dateElement.textContent = generateLastEdited(note.updatedAt);
 
 noteTitleElement.addEventListener('input', function(e) {
     note.title = e.target.value;
+    note.updatedAt = moment().valueOf();
+    dateElement.textContent = generateLastEdited(note.updatedAt);
     saveNotes(notes);
 });
 
 noteBodyElement.addEventListener('input', function(e) {
     note.body = e.target.value;
+    note.updatedAt = moment().valueOf();
+    dateElement.textContent = generateLastEdited(note.updatedAt);
     saveNotes(notes);
 });
 
@@ -43,6 +49,7 @@ window.addEventListener('storage', function(e) {
 
         noteTitleElement.value = note.title;
         noteBodyElement.value = note.body;
+        dateElement.textContent = generateLastEdited(note.updatedAt);
     }
 });
 
